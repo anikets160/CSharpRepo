b8cc91608148cfd14fa54a94a1117f1a4bfb068a
Aniket S <anikets160@gmail.com>
2021-05-23 20:36:53 +0530

    First Commit

---
+++ MyFirstWebApp/TestProject/Tests/TestClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestProject.Tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void Test1()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://www.testapp.com/");
+
+            Thread.Sleep(5000);
+
+            driver.Quit();
+        }
+    }
+}
